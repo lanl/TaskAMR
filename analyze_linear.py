6dcd3c916238c17d988900526147c3afeeb70b3f
Jonathan Pietarila Graham <jpietarilagraham@gmail.com>
2019-02-28 16:55:59 -0700

    new contractor

--- analyze_linear.py
+++ analyze_linear.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python
-# Copyright (c) 2018, Los Alamos National Security, LLC
-# All rights reserved.
 #
-# Copyright 2018. Los Alamos National Security, LLC. This software was produced under
-# U.S. Government contract DE-AC52-06NA25396 for Los Alamos National Laboratory (LANL),
-# which is operated by Los Alamos National Security, LLC for the U.S. Department of
-# Energy. The U.S. Government has rights to use, reproduce, and distribute this
-# software.  NEITHER THE GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY, LLC MAKES ANY
-# WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.
+# Copyright (c) 2018, Triad National Security, LLC
+# All rights reserved.
+# 
+# This program was produced under U.S. Government contract 89233218CNA000001 for
+# Los Alamos National Laboratory (LANL), which is operated by Triad National
+# Security, LLC for the U.S. Department of Energy/National Nuclear Security
+# Administration.
+# 
+# THIS SOFTWARE IS PROVIDED BY TRIAD NATIONAL SECURITY, LLC AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL TRIAD NATIONAL SECURITY, LLC OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# 
 # If software is modified to produce derivative works, such modified software should be
 # clearly marked, so as not to confuse it with the version available from LANL.
-#
 import numpy as np
 import argparse
 
